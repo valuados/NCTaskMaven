e18f466f55e872ad9604f96fcb887a042d12c4db
valuados <valuados@gmail.com>
2016-10-23 12:30:22 +0300

    Initial commit

---
+++ dao/src/main/java/by/netcracker/stone/connection/СonnectionGiver.java
@@ -0,0 +1,42 @@
+package by.netcracker.stone.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by valua on 10/13/2016.
+ */
+public class СonnectionGiver {
+    public static final String URL="jdbc:mysql://localhost:3306/stone";
+    //"jdbc:sqlite:E:/My Programms/Netcracker/TaskTwo/web/db/database.db";
+    public static final String USER="root";
+    public static final String PASS="";
+
+    private static СonnectionGiver instance = new СonnectionGiver();
+
+    public СonnectionGiver() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            System.err.println("couldn't find Driver. exiting");
+            System.exit(1);
+        }
+    }
+
+    public static СonnectionGiver getInstance() {
+        return instance;
+    }
+
+    public Connection getConnection() {
+        Connection cnt = null;
+        try {
+            cnt = DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException ex) {
+            System.err.println("couldn't create a connection. exiting" + ex);
+            System.exit(1);
+        }
+        return cnt;
+    }
+
+}
